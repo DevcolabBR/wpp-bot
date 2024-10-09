a843a3ff09d06aedc2d41021160ce9c128faf408
r0bert <robertdsgabriel@gmail.com>
2024-10-09 15:41:05 -0300

    Update bot version and fix default payment due date

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #!usr/bin/env python3
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 __author__ = "r0bert,   ,   "
 __license__ = "Unlicense"
 
@@ -11,15 +11,15 @@ from urllib.parse import quote
 import webbrowser
 from time import sleep
 import pyautogui
-import pillow
+from datetime import datetime
+
 #%%
 webbrowser.open("https://web.whatsapp.com")
-sleep(5)
+sleep(18)
 
 # importando tabelas
 workbook = openpyxl.load_workbook("tabela.xlsx")
 pagina_clientes = workbook["Sheet1"]  # nome da página no libreoficce
-
 
 # %%
 def corrigir_numero(numero):
@@ -45,32 +45,33 @@ def corrigir_numero(numero):
 
     return numero
 
+# Definindo um valor padrão de vencimento
+vencimento_padrao = datetime.strptime('01/01/2024', '%d/%m/%Y')
 
 # Lendo os números de telefone da tabela e aplicando a função corrigir_numero
 for linha in pagina_clientes.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = str(linha[1].value)  # Convertendo telefone para string
-    vencimento = linha[2].value
-
     telefone_corrigido = corrigir_numero(telefone)
+
+    # Usando o valor padrão de vencimento
+    vencimento = vencimento_padrao
+
     mensagem = f"""Olá {nome}, seu boleto vence no dia {vencimento.strftime('%d/%m/%Y')}. Por favor, pagar no link
     https://www.link_do_pagamento.com """
-    # print(nome)
-    # print(telefone_corrigido)
-    # print(vencimento)
-
-
-try:
+    
+    try:
         link_mensagem_wpp = f'https://web.whatsapp.com/send?phone={telefone_corrigido}&text={quote(mensagem)}'
         webbrowser.open(link_mensagem_wpp)
-        sleep(10)
-        seta = pyautogui.locateCenterOnScreen('seta.jpeg')
-        sleep(5)
-        pyautogui.click(seta[0], seta[1])
+        sleep(15)
+        pyautogui.press('enter')
+        #seta = pyautogui.locateCenterOnScreen('seta.jpeg')
+        sleep(3)
+        #pyautogui.click(seta[0], seta[1])
         sleep(5)
         pyautogui.hotkey('ctrl', 'w') # pra fechar a aba
 
-except: 
-    print("Não foi possível enviar mensagem para {nome}")
-    with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo_erros:
-        arquivo_erros.write(f"{nome};{telefone}\n")
+    except Exception as e:
+        print(f"Não foi possível enviar mensagem para {nome}: {e}")
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo_erros:
+            arquivo_erros.write(f"{nome};{telefone}\n")
